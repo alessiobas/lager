39b87602ee28d90fee067a921908b37b957c3159
Alessio Basile <alessio.basile.jr@gmail.com>
2022-05-02 19:37:48 +0200

    Added FlashMessage for email and password verification

--- components/auth/AuthFields.tsx
+++ components/auth/AuthFields.tsx
@@ -1,7 +1,33 @@
 import { View, Text, TextInput, Button } from "react-native";
 import { Typography, Forms, Base } from '../../styles';
+import { showMessage } from 'react-native-flash-message';
 
 export default function AuthFields({ auth, setAuth, title, submit, navigation}) {
+
+    function validatePassword(text: string) {
+        const pattern = /^(=?.\d)(?=.[a-z])(?=.*[A-Z]).{4,}$/
+
+        if (!text.match(pattern)) {
+            showMessage({
+                message: "Ej giltigt lösenord",
+                description: "Lösenordet måste innehålla minst 4 tecken, små och stora bokstäver och minst en siffra",
+                type: "warning"
+            });
+        }
+    }
+
+    function validateEmail(text: string) {
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if (!text.match(pattern)) {
+            showMessage({
+                message: "Ej giltig email",
+                description: "Email måste vara formaterad enligt aaaa@aaa.aa",
+                type: "warning"
+            });
+        }
+    }
+
     return (
         <View style={Base.container}>
             <Text style={Typography.header2}>{title}</Text>
@@ -9,6 +35,7 @@ export default function AuthFields({ auth, setAuth, title, submit, navigation}) 
             <TextInput
                 style={Forms.input}
                 onChangeText={(content: string) => {
+                    validateEmail(content)
                     setAuth({ ...auth, email: content })
                 }}
                 value={auth?.email}
@@ -20,6 +47,7 @@ export default function AuthFields({ auth, setAuth, title, submit, navigation}) 
             <TextInput
                 style={Forms.input}
                 onChangeText={(content: string) => {
+                    validatePassword(content)
                     setAuth({ ...auth, password: content })
                 }}
                 value={auth?.password}
